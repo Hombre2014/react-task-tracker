9df15e0f0f4ef388007a005e79d43ec6680197d9
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-01-14 19:47:59 +0100

    Full App with HTTP server for pruction installed

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,12 @@
-const Task = () => {
+import { FaTimes } from 'react-icons/fa';
+
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div>
-      
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onMouseDown={() => onToggle(task.id)} >
+      <h3>{task.text}{' '}
+      <FaTimes style={{ color: 'red', cursor: 'pointer'}}
+        onClick={() => onDelete(task.id)} /></h3>
+      <p>{task.day}</p>
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,15 @@
-// Using Hooks with useState
-const Tasks = ({ tasks }) => {
+import Task from "./Task";
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
-    // To add a new tasks into the array, you do not use tasks.push(), but setTasks instead
-    // setTasks([...tasks, {}])
     <>
-      {tasks.map((task) => (<h3 key={task.id} >{task.text}</h3>))}
+      {tasks.map((task) => (
+      <Task
+        key={task.id}
+        task={task}
+        onDelete={onDelete}
+        onToggle={onToggle}
+      />))}
     </>
   )
 }
